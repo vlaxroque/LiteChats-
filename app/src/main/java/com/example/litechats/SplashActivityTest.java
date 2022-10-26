de000626b7e70469baa285618cf8274485548fc5
vlaxroque <vin.ombongi@gmail.com>
2022-10-26 18:01:05 +0300

    my second commit

---
+++ app/src/main/java/com/example/litechats/SplashActivityTest.java
@@ -0,0 +1,60 @@
+package com.example.litechats;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import com.example.litechats.R;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class SplashActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<SplashActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(SplashActivity.class);
+
+    @Test
+    public void splashActivityTest() {
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction button = onView(
+                allOf(withId(R.id.btnStudentLogin), withText("STUDENT"),
+                        withParent(withParent(withId(android.R.id.content))),
+                        isDisplayed()));
+        button.check(doesNotExist());
+
+        ViewInteraction button2 = onView(
+                allOf(withId(R.id.btnUniversityLogin), withText("UNIVERSITY"),
+                        withParent(withParent(withId(android.R.id.content))),
+                        isDisplayed()));
+        button2.check(matches(isDisplayed()));
+
+        ViewInteraction button3 = onView(
+                allOf(withId(R.id.btnUniversityLogin), withText("UNIVERSITY"),
+                        withParent(withParent(withId(android.R.id.content))),
+                        isDisplayed()));
+        button3.check(matches(isDisplayed()));
+    }
+}
